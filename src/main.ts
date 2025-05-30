b56bf9261e0a8696034627f95ad8d17cc7aae43d
Carlos David <romao.carlosdavi@gmail.com>
2025-05-30 15:02:40 -0300

    Adiciona módulos Admin e Client, configura o bootstrap da aplicação e atualiza dependências no package.json e package-lock.json.

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
-async function bootstrap() {}
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AdminModule } from './admin/admin.module';
+import { ClientModule } from './client/client.module';
+
+async function bootstrap() {
+  const adminApp = await NestFactory.create(AdminModule);
+
+  adminApp.setGlobalPrefix('/api/admin');
+  adminApp.enableCors();
+  adminApp.useGlobalPipes(new ValidationPipe());
+  await adminApp.listen(3001);
+
+  const clientApp = await NestFactory.create(ClientModule);
+  clientApp.setGlobalPrefix('/api/clientes');
+  clientApp.enableCors();
+  clientApp.useGlobalPipes(new ValidationPipe());
+  await clientApp.listen(3002);
+}
 bootstrap();
